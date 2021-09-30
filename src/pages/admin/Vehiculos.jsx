3b24c015b3c68597879a4dadfda011e635be7645
Luz González <legonzalez949misena.edu.co>
2021-09-30 12:58:54 -0500

    interaccion creando formularios y tablas de vehiculos, no me sale como al profe video 15

--- src/pages/admin/Vehiculos.jsx
+++ src/pages/admin/Vehiculos.jsx
@@ -1,56 +1,108 @@
 import React, { useEffect, useState } from 'react';
 
-//realizar un formulario que le pida al usuario 
+const vehiculosBackend= [
+    {
+         nombre:'corolla',
+         marca:'toyota',
+         modelo: '2014'
+    },
+    {
+         nombre:'Sandero',
+         marca:'Toyota',
+         modelo: '2014'
+    },
+    {
+        nombre:'Douster',
+        marca:'Renauld',
+        modelo: '2014'
+   },
+]
 
 const Vehiculos = () => {
-    const [edad,setEdad] = useState(0);
-
-    const [esMenorDeEdad, setEsMenorDeEdad]= useState(false);
-    const [mostrarCamposAdicionales, setMostrarCamposAdicionales]= useState(false);
+    const [mostrarTabla, setMostrarTabla] = useState(true);
+    const [vehiculos, setVehiculos] = useState([]);
+    const [textoBoton, setTextoBoton] = useState('crear nuevo vehiculo');
 
     useEffect(()=>{
-        if(edad >= 18){
-            setEsMenorDeEdad(false);
+        //obtener lista de vehiculos desde el backend
+        setVehiculos([vehiculosBackend]);
+    },[]);
+
+    useEffect(() => {
+        if (mostrarTabla) {
+            setTextoBoton("crear nuevo vehiculo")
         }
-        else{
-            setEsMenorDeEdad(true);
+        else {
+            setTextoBoton("Mostrar todos los vehiculos");
         }
-    },[edad])
+    }, [mostrarTabla]);
 
     return (
-   /* Hay 2 formas de manejar la reactividad (huts)=> son funciones */
-   <from className='flex flex-col '>
-       <h2>Formulario de Creacion de Vehiculos:</h2>
-       <label htmlFor='edad'>
-           Por favor ingrese su edad
-           <input 
-            value={edad} onChange={(e)=>
-            {setEdad(e.target.value);
-            }} 
-            className='border border-gray-600' 
-            name= 'edad' 
-            type='number'
-            />
-       </label>
-       {esMenorDeEdad ? ( 
-          <span className='text-3xl text-red-500'>
-             !USTED ES MENOR DE EDAD, no puede hacer pagos¡
-          </span>
-          ):(
-          <span className='text-3xl text-green-500'>
-              !USTED ES MAYOR DE EDAD, SI puede hacer pagos¡
-          </span>
-        )}  
-        <button onClick={()=> setMostrarCamposAdicionales(!mostrarCamposAdicionales)} type='button'  className='text-white bg-indigo-500'>Mostrar Campos Adicionales</button>  
-        {mostrarCamposAdicionales &&
-        <div>
-            <input className='border bg-gray-400 my-2 p-3' placeholder='dato nuevo' type='text' />
-            <input  className='border bg-gray-400 my-2 p-3' placeholder='dato nuevo' type='text' />
-            <input className='border bg-gray-400 my-2 p-3' placeholder='dato nuevo'  type='text' />
-            <input className='border bg-gray-400 my-2 p-3' placeholder='dato nuevo' type='text' />
-        </div>}   
-   </from>
+        <div className='flex h-full w-full flex-col items-center justify-start p-8'>
+            <div className='flex flex-col'>
+                <h2 className='text-3xl font-extrabold text-gray-800'>pagina de administracion de vehiculos</h2>
+                <button onClick={() => {
+                    setMostrarTabla(!mostrarTabla);
+                }}
+
+                    className="text-white bg-indigo-500 p-5 rounded-full m-6 w-28 self-end"
+                >
+                    {textoBoton}
+                </button>
+            </div>
+            {mostrarTabla ? (
+            <TablaVehiculos listaVehiculos={vehiculos} /> 
+            ): (
+            <FormularioCreacionVehiculos />
+            )}
+        </div>
     );
 };
+const TablaVehiculos = ({listaVehiculos}) => {
+    useEffect(()=>{
+    console.log('este es el listado de vehiculos en el componente de tabla', listaVehiculos);
+    },[listaVehiculos]);
+    return ( 
+    <div className='flex flex-col items-center justify-center'>
+        <h2 className='text-2xl font-extrabold text-gray-800' >Todos los vehiculos</h2>
+    <table>
+        <thead>
+          {/* ponemos los titulos */}
+          <tr>
+              <th>Nombre del Vehiculo</th>
+              <th>Marca del Vehiculo</th>
+              <th>Modelo del Vehiculo</th>
+          </tr>
+        </thead>
+        <tbody>
+            {listaVehiculos.map((vehiculo)=>{
+                return(
+                    <tr>
+                    <td>{vehiculo.nombre}</td>
+                    <td>{vehiculo.marca}</td>
+                    <td>{vehiculo.modelo}</td>
+                </tr>
+                );
+            })}
+        </tbody>
+      </table>
+    </div>
+    );
+};
+const FormularioCreacionVehiculos = () => {
+    return (
+        <div className='flex flex-col items-center justify-center'>
+            <h2 className='text-2xl font-extrabold text-gray-800'>Crear nuevo Vehiculo</h2>
+            <form className='grid grid-cols-2 '>
+                <input className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2' type="text" />
+                <input className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2' type="text" />
+                <input className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2' type="text" />
+                <input className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2' type="text" />
+                <button className='col-span-2 bg-green-400 p-2 rounded-full shadow-md hover:bg-green-600 text-white'>Guardar Vehiculo
+                </button>
+            </form>
+        </div>
+    );
+}
 
 export default Vehiculos
